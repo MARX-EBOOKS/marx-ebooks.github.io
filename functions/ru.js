63956956b9af3b4f58d4095f1d9d318958143b81
lldd1226 <60905617+lldd1226@users.noreply.github.com>
2026-02-07 15:21:16 +0800

    redirects

--- functions/ru.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest({ params, request }) {
-  const path = params.path.join("/");
-  const upstream = `https://vilpss.pages.dev/${path}`;
-  const resp = await fetch(upstream);
-  return new Response(resp.body, {
-    status: resp.status,
-    headers: resp.headers,
-  });
-}
