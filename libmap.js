8ca24c5ead43353ca7c858edd5e18e0f710bc14b
lldd1226 <60905617+lldd1226@users.noreply.github.com>
2026-05-20 16:46:10 +0800

    Page number display update

--- libmap.js
+++ libmap.js
@@ -210,7 +210,7 @@ window.LIBRARY_CONFIG = [
     {
         id: 'mega',
         label: 'MEGA2',
-        title: 'KARL MARX FRIEDRICH ENGELS GESAMTAUSGABE (MEGA)',
+        title: 'KARL MARX FRIEDRICH ENGELS GESAMTAUSGABE (von BBAW)',
         desc: 'ZWEITE ABTEILUNG',
         basePath: '/MEGA/',
         badge: 'DE',
@@ -525,7 +525,8 @@ window.LIBRARY_CONFIG = [
         basePath: '/docs/HEGEL/',
         stylesheets: ['/docs/HEGEL/hegel.css'],
         badge: 'DE',
-        path: '/docs/HEGEL/index.html#de',
+        path: '/docs/HEGEL/index.html#de ',
+        citation: { prefix: 'G.W.F. Hegel: Werke', title: 'Georg Wilhelm Friedrich: Werke in zwanzig Bänden', publisher: 'Frankfurt am Main: Suhrkamp',year:'1979ff' },
         groups: [
             {
                 label: 'Hegel Werke 1-20',
